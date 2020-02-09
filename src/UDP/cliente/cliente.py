bc11660a2ad63b6486f1b17f9a4692f884a2ddd3
luizfernando <luiz-fsilva37@hotmail.com>
2020-02-09 12:53:22 -0300

    Atualização sockets UDP

--- src/UDP/cliente/cliente.py
+++ src/UDP/cliente/cliente.py
@@ -3,23 +3,23 @@ import sys
 
 HOST = "localhost"
 HOST_PORTA = 5001
-BUFFER_TAMANHO = 4096
+TAMANHO_BUFFER = 4096
 
 nome_arquivo = sys.argv[1]
 
-print(f"[*] Conectando cliente {HOST}:{HOST_PORTA}.")
+print(f"[*] Conectando cliente {HOST}:{HOST_PORTA}")
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print(f"[*] Cliente conectado.")
 
 socket.sendto(nome_arquivo.encode(), (HOST, HOST_PORTA))
-print(f"[+] Enviando o arquivo: {nome_arquivo}.")
+print(f"[+] Enviando o arquivo: {nome_arquivo}")
 
 a = open(nome_arquivo, "rb")
-arquivo = a.read(BUFFER_TAMANHO)
+arquivo = a.read(TAMANHO_BUFFER)
 
 while(arquivo):
-    if(socket.sendto(arquivo.encode(), (HOST, HOST_PORTA))):
-        arquivo = a.read(BUFFER_TAMANHO)
+    if(socket.sendto(arquivo, (HOST, HOST_PORTA))):
+        arquivo = a.read(TAMANHO_BUFFER)
 
 print(f"[+] Arquivo {nome_arquivo} enviado!")
 
--- src/UDP/servidor/servidor.py
+++ src/UDP/servidor/servidor.py
@@ -3,17 +3,17 @@ import select
 import os
 
 SERVER_HOST = "0.0.0.0"
-SERVER_PORTA = 5001
-BUFFER_TAMANHO = 4096
+PORTA_SERVER = 5001
+TAMANHO_BUFFER = 4096
 TIME_OUT = 2
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket.bind((SERVER_HOST, SERVER_PORTA))
+socket.bind((SERVER_HOST, PORTA_SERVER))
 
 while True:
-    print(f"[*] Servidor UDP ativo em: {SERVER_HOST}:{SERVER_PORTA}")
+    print(f"[*] Servidor UDP ativo em: {SERVER_HOST}:{PORTA_SERVER}")
     
-    arquivo, endereco = socket.recvfrom(BUFFER_TAMANHO)
+    arquivo, endereco = socket.recvfrom(TAMANHO_BUFFER)
     if arquivo:
         nome_arquivo = os.path.basename(arquivo)
         print(f"[+] Recebendo arquivo: {nome_arquivo}")
@@ -23,7 +23,7 @@ while True:
     while True:
         leitura = select.select([socket], [], [], TIME_OUT)
         if leitura[0]:
-            arquivo, endereco = socket.recvfrom(BUFFER_TAMANHO)
+            arquivo, endereco = socket.recvfrom(TAMANHO_BUFFER)
             a.write(arquivo)
         else:
             print(f"[+] Arquivo recebido: {nome_arquivo}!")
