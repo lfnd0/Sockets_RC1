91c169701ade7bdfd679cc579baff224916bf08e
luizfernando <luiz-fsilva37@hotmail.com>
2020-02-10 19:30:57 -0300

    Atualização socket UDP

--- src/UDP/cliente/cliente.py
+++ src/UDP/cliente/cliente.py
@@ -8,20 +8,20 @@ TAMANHO_BUFFER = 4096
 nome_arquivo = sys.argv[1]
 
 print(f"[*] Conectando cliente {HOST}:{HOST_PORTA}")
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print(f"[*] Cliente conectado.")
 
-socket.sendto(nome_arquivo.encode(), (HOST, HOST_PORTA))
+s.sendto(nome_arquivo.encode(), (HOST, HOST_PORTA))
 print(f"[+] Enviando o arquivo: {nome_arquivo}")
 
 a = open(nome_arquivo, "rb")
 arquivo = a.read(TAMANHO_BUFFER)
 
 while(arquivo):
-    if(socket.sendto(arquivo, (HOST, HOST_PORTA))):
+    if(s.sendto(arquivo, (HOST, HOST_PORTA))):
         arquivo = a.read(TAMANHO_BUFFER)
 
 print(f"[+] Arquivo {nome_arquivo} enviado!")
 
-socket.close()
+s.close()
 a.close()
--- src/UDP/servidor/servidor.py
+++ src/UDP/servidor/servidor.py
@@ -7,13 +7,13 @@ PORTA_SERVER = 5001
 TAMANHO_BUFFER = 4096
 TIME_OUT = 2
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket.bind((SERVER_HOST, PORTA_SERVER))
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((SERVER_HOST, PORTA_SERVER))
 
 while True:
-    print(f"[*] Servidor UDP ativo em: {SERVER_HOST}:{PORTA_SERVER}")
+    print(f"[*] Servidor ativo em: {SERVER_HOST}:{PORTA_SERVER}")
     
-    arquivo, endereco = socket.recvfrom(TAMANHO_BUFFER)
+    arquivo, endereco = s.recvfrom(TAMANHO_BUFFER)
     if arquivo:
         nome_arquivo = os.path.basename(arquivo)
         print(f"[+] Recebendo arquivo: {nome_arquivo}")
@@ -21,9 +21,9 @@ while True:
     a = open(arquivo, "wb")
 
     while True:
-        leitura = select.select([socket], [], [], TIME_OUT)
+        leitura = select.select([s], [], [], TIME_OUT)
         if leitura[0]:
-            arquivo, endereco = socket.recvfrom(TAMANHO_BUFFER)
+            arquivo, endereco = s.recvfrom(TAMANHO_BUFFER)
             a.write(arquivo)
         else:
             print(f"[+] Arquivo recebido: {nome_arquivo}!")
